4e3c11f9551b49eade57d11792a94294b56c4f94
Алена Карпова <alenapancenko00@gmail.com>
2023-01-17 14:31:50 +0300

    Сделала 1 дз

--- SwiftUIDZ/SwiftUIDZ/ContentView.swift
+++ SwiftUIDZ/SwiftUIDZ/ContentView.swift
@@ -8,19 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isAlertExample = false
+    @State var isTwoButtonAlertExample = false
+    @State var isActionSheetExample = false
+    @State var isActionSheetTwoExample = false
+    @State var oldValueText = "Пример Alert c 2 кнопками и логикой"
+    @State var textColor = Color.black
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Пример Alert").foregroundColor(Color.black)
+            showAlert().padding(15)
+               
+            Text(oldValueText)
+            showTwoButtonAlert().padding(15)
+            Text("Пример ActionSheet")
+            showThreeButtonAlert().padding(15)
+            Text("Пример ActionSheet c кнопками и логикой").foregroundColor(textColor)
+            showButtonAсtionSheet().padding(15)
         }
-        .padding()
     }
+    
+    fileprivate func showAlert() -> some View {
+        return Button(action: {
+            isAlertExample = true
+        },
+                      label: {
+            Text("Показать")
+        })
+        .background(Color.blue)
+        .foregroundColor(Color.white)
+        .cornerRadius(2)
+        .alert(isPresented: $isAlertExample) {
+            Alert(title: Text("Пример алерта"))
+        }
+    }
+    
+    fileprivate func showTwoButtonAlert() -> some View {
+        return Button(action: {
+            isTwoButtonAlertExample = true
+        },
+                      label: {
+            Text("Показать")
+        })
+        .foregroundColor(Color.white)
+        .cornerRadius(5)
+        .background(Color.brown)
+        
+        .alert(isPresented: $isTwoButtonAlertExample) {
+            Alert(title: Text("Изменить текст кнопки?"), primaryButton: .default(Text("Меняем"), action: {
+                oldValueText = "Новый текст"
+            }), secondaryButton: .cancel())
+        }
+    }
+    
+    fileprivate func showThreeButtonAlert() -> some View {
+        return Button(action: {
+            isActionSheetExample = true
+        },
+                      label: {
+            Text("Показать")
+        })
+        .foregroundColor(Color.white)
+        .cornerRadius(5)
+        .background(Color.red)
+        
+        .actionSheet(isPresented: $isActionSheetExample, content: { ActionSheet(title: Text("А вот он я"))})
+    }
+    
+    fileprivate func showButtonAсtionSheet() -> some View {
+        return Button(action: {
+            isActionSheetTwoExample = true
+        },
+                      label: {
+            Text("Показать")
+        })
+        .foregroundColor(Color.white)
+        .cornerRadius(5)
+        .background(Color.green)
+        
+        .actionSheet(
+            isPresented: $isActionSheetTwoExample, content: {
+                ActionSheet(title: Text("Внимание!"),
+                            message: Text("Сейчас будем менять цвета!"),
+                            buttons: [.default(Text("меняем цвет надписи"), action: { textColor = .green }),
+                                      .cancel()
+                                                                                                                                                                   ])})
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
         ContentView()
     }
 }
