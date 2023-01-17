8ca2b287cca3d871f15f9b9ea15e882a3135fba7
Алена Карпова <alenapancenko00@gmail.com>
2023-01-17 11:54:57 +0300

    init main

---
+++ SwiftUIDZ/SwiftUIDZ/SwiftUIDZApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDZApp.swift
+//  SwiftUIDZ
+//
+//  Created by Алена Панченко on 17.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
