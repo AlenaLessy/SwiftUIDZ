f0d08fceb6ab621881c0de4eee97b68ec622f921
Алена Карпова <alenapancenko00@gmail.com>
2023-01-18 11:19:43 +0300

    рефакторинг и исправления

--- SwiftUIDZ/SwiftUIDZ/ContentView.swift
+++ SwiftUIDZ/SwiftUIDZ/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/// Экран денежных операций
 struct ContentView: View {
     
     // MARK: - Private Constants
@@ -14,34 +15,48 @@ struct ContentView: View {
         static let walletText = "Кошелек"
         static let profileText = "Профиль"
         static let currentBalanceText = "Текущий баланс:"
-        static let showSettings = "Показать настройки"
-        static let rub = "рублей"
-        static let refillTheBalanceButtonTitle = "Пополнить"
+        static let showSettingsText = "Показать настройки"
+        static let rubText = "рублей"
+        static let refillTheBalanceButtonTitleText = "Пополнить"
         static let actionSheetRefillTheBalanceText = "Баланс пополнен"
         static let actionSheetWriteOffFundsText = "Деньги списаны"
-        static let actionSheetInsufficientFunds = "Недостаточно средств"
+        static let actionSheetInsufficientFundsText = "Недостаточно средств"
         static let emptyString = ""
-        static let openTwoCheck = "Второй счет открыт"
-        static let openCheckText = "Открыть счет"
-        static let refillTheBalanceAlertButton = "Пополнить на 50 рублей?"
-        static let transfersMoneyAlertTitle = "Переводы"
-        static let transfersMoneyAlertButton = "Перевести"
-        static let transferMoneyAlertTitle = "Перевести 50 рублей?"
+        static let openTwoBankAccountText = "Второй счет открыт"
+        static let openBankAccountText = "Открыть счет"
+        static let refillTheBalanceAlertButtonText = "Пополнить на 50 рублей?"
+        static let transfersMoneyAlertTitleText = "Переводы"
+        static let transfersMoneyAlertButtonText = "Перевести"
+        static let transferMoneyAlertTitleText = "Перевести 50 рублей?"
         
-        
+        static let zeroNumber = 0
+        static let zeroCGFloatNumber: CGFloat = 0
+        static let fiftyNumber = 50
+        static let walletTextSpacerHeightNumber: CGFloat = 20
+        static let transfersMoneyButtonSpacerHeightNumber: CGFloat = 10
+        static let refillTheBalanceButtonSpacerHeightNumber: CGFloat = 150
+        static let profileTextSpacerHeightNumber: CGFloat = 20
+        static let openBankAccountButtonSpacerHeightNumber: CGFloat = 100
+        static let buttonWidthNumber: CGFloat = 100
+        static let buttonHeightNumber: CGFloat = 30
+        static let cornerRadiusNumber: CGFloat = 10
+        static let animationResponseNumber = 0.5
+        static let animationDampingFractionNumber = 0.7
+        static let animationBlendDurationNumber = 0.3
+        static let balanceTwoOffsetY: CGFloat = 30
+        static let biasViewNumber: CGFloat = 200
+        static let biasTextNumber: CGFloat = 100
     }
     
     @State var isOnToogle = false
-    @State var balance = 0
-    @State var balanceTwo = 50
+    @State var balance = Constants.zeroNumber
+    @State var balanceTwo = Constants.fiftyNumber
     @State var isRefillTheBalanceAlert = false
     @State var isBalanceToppedUp = false
     @State var actionSheetText = Constants.emptyString
     @State var isTransfersMoneyAlert = false
-    @State var isCreateСheck = false
-    @State var openTwoCheck = Constants.emptyString
-    
-    
+    @State var isCreateBankAccount = false
+    @State var openTwoBankAccount = Constants.emptyString
     
     var body: some View {
         VStack {
@@ -49,54 +64,52 @@ struct ContentView: View {
                 HStack {
                     VStack {
                         Text(Constants.walletText)
-                        Spacer().frame(height: 20)
+                        Spacer().frame(height: Constants.walletTextSpacerHeightNumber)
                         transfersMoneyButton()
-                        Spacer().frame(height: 10)
+                        Spacer().frame(height: Constants.transfersMoneyButtonSpacerHeightNumber)
                         refillTheBalanceButton()
-                        Spacer().frame(height: 150)
+                        Spacer().frame(height: Constants.refillTheBalanceButtonSpacerHeightNumber)
                         Text(Constants.profileText)
-                        Spacer().frame(height: 20)
-                        openCheckButton()
-                        Spacer().frame(height: 100)
+                        Spacer().frame(height: Constants.profileTextSpacerHeightNumber)
+                        openBankAccountButton()
+                        Spacer().frame(height: Constants.openBankAccountButtonSpacerHeightNumber)
                     }
                     Spacer()
                 }
                 Spacer()
-                RoundedRectangle(cornerRadius: 10).fill(Color.yellow).offset(x: isOnToogle ? 200: 0)
+                RoundedRectangle(cornerRadius: Constants.cornerRadiusNumber).fill(Color.yellow).offset(x: isOnToogle ? Constants.biasViewNumber : Constants.zeroCGFloatNumber)
                 VStack {
-                    makeCheck(balance: balance)
+                    makeBankAccount(balance: balance)
                     
-                    if isCreateСheck {
-                        makeCheck(balance: balanceTwo).offset(y: 30)
+                    if isCreateBankAccount {
+                        makeBankAccount(balance: balanceTwo).offset(y: Constants.balanceTwoOffsetY)
                     }
                 }
-                .offset(x: isOnToogle ? 100: 0)
+                .offset(x: isOnToogle ? Constants.biasTextNumber : Constants.zeroCGFloatNumber)
             }
-            Toggle(isOn: $isOnToogle, label: { Text(Constants.showSettings)
+            Toggle(isOn: $isOnToogle, label: { Text(Constants.showSettingsText)
             }).padding()
-        }.animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3))
+        }.animation(.spring(response: Constants.animationResponseNumber, dampingFraction: Constants.animationDampingFractionNumber, blendDuration: Constants.animationBlendDurationNumber))
     }
     
-    fileprivate func makeCheck(balance: Int
-    ) -> some View {
+    fileprivate func makeBankAccount(balance: Int) -> some View {
         return VStack {
             Text(Constants.currentBalanceText)
-            Text("\(balance) \(Constants.rub)")
+            Text("\(balance) \(Constants.rubText)")
         }
         .actionSheet(isPresented: $isBalanceToppedUp) {
             ActionSheet(title: Text(actionSheetText))
-            
         }
     }
     
-    fileprivate func openCheckButton() -> Button<Text> {
+    fileprivate func openBankAccountButton() -> Button<Text> {
         return Button {
-            if !isCreateСheck {
-                isCreateСheck = true
-                openTwoCheck = Constants.openTwoCheck
+            if !isCreateBankAccount {
+                isCreateBankAccount = true
+                openTwoBankAccount = Constants.openTwoBankAccountText
             }
         } label: {
-            Text(Constants.openCheckText)
+            Text(Constants.openBankAccountText)
         }
     }
     
@@ -104,18 +117,18 @@ struct ContentView: View {
         return Button {
             isRefillTheBalanceAlert = true
         } label: {
-            Text(Constants.refillTheBalanceButtonTitle)
+            Text(Constants.refillTheBalanceButtonTitleText)
         }
         .alert(isPresented: $isRefillTheBalanceAlert) {
-            Alert(title: Text(Constants.refillTheBalanceButtonTitle), primaryButton: .default(Text(Constants.refillTheBalanceAlertButton), action: {
-                balance += 50
+            Alert(title: Text(Constants.refillTheBalanceButtonTitleText), primaryButton: .default(Text(Constants.refillTheBalanceAlertButtonText), action: {
+                balance += Constants.fiftyNumber
                 isBalanceToppedUp = true
                 actionSheetText = Constants.actionSheetRefillTheBalanceText
                 
                             }), secondaryButton: .cancel())
         }
 
-        .frame(width: 100, height: 30)
+        .frame(width: Constants.buttonWidthNumber, height: Constants.buttonHeightNumber)
         .background(Color.green)
         .foregroundColor(Color.white)
     }
@@ -124,28 +137,27 @@ struct ContentView: View {
         return Button {
             isTransfersMoneyAlert = true
         } label: {
-            Text(Constants.transfersMoneyAlertTitle)
+            Text(Constants.transfersMoneyAlertTitleText)
         }
         .alert(isPresented: $isTransfersMoneyAlert) {
-            Alert(title: Text(Constants.transferMoneyAlertTitle), primaryButton: .default(Text(Constants.transfersMoneyAlertButton), action: {
-                if balance >= 50 {
-                    balance -= 50
-                    balanceTwo += 50
+            Alert(title: Text(Constants.transferMoneyAlertTitleText), primaryButton: .default(Text(Constants.transfersMoneyAlertButtonText), action: {
+                if balance >= Constants.fiftyNumber {
+                    balance -= Constants.fiftyNumber
+                    balanceTwo += Constants.fiftyNumber
                     isBalanceToppedUp = true
                     actionSheetText = Constants.actionSheetWriteOffFundsText
                     
                 } else {
                     isBalanceToppedUp = true
-                    actionSheetText = Constants.actionSheetInsufficientFunds
+                    actionSheetText = Constants.actionSheetInsufficientFundsText
                 }
                             }), secondaryButton: .cancel())
         }
 
-        .frame(width: 100, height: 30)
+        .frame(width: Constants.buttonWidthNumber, height: Constants.buttonHeightNumber)
         .background(Color.green)
         .foregroundColor(Color.white)
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
